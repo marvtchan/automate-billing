74e015fc482e977521aa571955f01c20caa0f573
marvtchan <49408616+marvtchan@users.noreply.github.com>
2019-04-19 19:26:50 -0700

    Update companiesunique.py

--- companiesunique.py
+++ companiesunique.py
@@ -3,26 +3,13 @@ import numpy as np
 from pandas import ExcelFile
 from pandas import ExcelWriter
 
-import os
-
-os.chdir('/Users/marvinchan/Documents/PythonProgramming')
-
-pd.set_option('display.max_columns', None)
-
 pd.options.display.max_rows = 999
-
-print(os.getcwd())
-
-# show current working directory
-
-print(os.listdir(os.getcwd()))
 
 xls = pd.ExcelFile('conferencerooms.xls')
 df1 = pd.read_excel(xls, 'Sheet 2')
 
 global bill_hours
 bill_hours = []
-
 
 users = df1['email'].str.split("@", expand= True)
 users.columns = ['user','domain']
