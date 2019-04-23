eee6423d95283cb91c206323f5ea15b751d2c1db
marvtchan <49408616+marvtchan@users.noreply.github.com>
2019-04-22 20:06:21 -0700

    Update combine.py

--- combine.py
+++ combine.py
@@ -9,18 +9,22 @@ print(os.listdir(os.getcwd()))
 
 path = os.getcwd()
 
+#list all files in current working directory
 files = os.listdir(path)
 
+#function for looping through all files ending in bill.xlsx
 files_xls = [f for f in files if f[-9:] == 'bill.xlsx']
 
 print(files_xls)
 
 df = pd.DataFrame()
 
+#loop through sheet 1 in each file
 for f in files_xls:
 	data = pd.read_excel(f, 'Sheet1')
 	df = df.append(data)
 
+#add companies and sum
 df['Total'] = df.groupby(['Companies'])['Hours'].transform('sum')
 
 new_df = df.drop_duplicates(subset=['Companies'])
