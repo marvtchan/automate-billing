93cc2d6579dc522d1a010307d4160ff8fb2db531
marvtchan <49408616+marvtchan@users.noreply.github.com>
2019-04-22 19:57:08 -0700

    Update companiesunique.py

--- companiesunique.py
+++ companiesunique.py
@@ -3,33 +3,45 @@ import numpy as np
 from pandas import ExcelFile
 from pandas import ExcelWriter
 
+#display all rows in dataframe
 pd.options.display.max_rows = 999
 
+#import any excel file for parsing
 xls = pd.ExcelFile('conferencerooms.xls')
 df1 = pd.read_excel(xls, 'Sheet 2')
 
+#create variable for sum of hours billable
 global bill_hours
 bill_hours = []
 
-users = df1['email'].str.split("@", expand= True)
-users.columns = ['user','domain']
-companiesunique = users.domain.unique()
+#create variable for email domains that will be extracted
+global emails
+emails = []
 
-print(companiesunique)
+#function for extracting email domain by uniqueness
+def companies():
+		users = df1['email'].str.split("@", expand= True)
+		users.columns = ['user','domain']
+		emails = users.domain.unique()
+		return emails
 
+#intiating function to set emails variable
+companies()
+emails = companies()
+
+#parse files for billable hours
 def weekly_run():
-	for options in companiesunique:
+	for options in emails:
 		print(options)
 		results = df1[df1['email'].str.contains(options)]
 		companyhours = float(sum(results['Hours']))
 		bill_hours.append(companyhours)
 
 weekly_run()
-
+#double check
 print(bill_hours)
 
-companiesdf1 = pd.DataFrame({'Companies': companiesunique, 'Hours' : bill_hours})
+#final dataframe for unique domains and billable hours
+companiesdf1 = pd.DataFrame({'Companies': emails, 'Hours' : bill_hours})
 
 print(companiesdf1)
-
-
